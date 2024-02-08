62934d68cbcc0d1bc9401d7d5d25141426e8606c
Andrés RUBIO <rubio.ae086@gmail.com>
2024-02-08 06:48:15 +0100

    adding the task C

--- ROB305-TPs/TD1/mainTD1c.cpp
+++ ROB305-TPs/TD1/mainTD1c.cpp
@@ -1,58 +1,44 @@
-#include <iostream>
-#include <pthread.h>
+#include <unistd.h>
 #include <time.h>
+#include <iostream>
+#include <iomanip>
+#include <stdio.h>
 
 
-void* incrementer(void* v_stop) {
-        volatile bool* p_stop = (volatile bool*) v_stop;
-        double counter = 0.0;
-        while (not *p_stop) {
-            counter += 1.0;
-        }
-        std::cout << "Counter value = " << counter << std::endl;
-        return v_stop;
-    }
+timespec timespec_now(){
+	timespec time_now;
+	clock_gettime(CLOCK_REALTIME, &time_now);
+	return time_now;
+}
 
-void* incr(unsigned int nLoops, double* pCounter){
-    for (unsigned int i = 0; i < nLoops; i++){
+
+void incr(unsigned int nLoops, double* pCounter)
+{
+    for(unsigned int i = 0; i < nLoops; ++i)
+    {
         *pCounter += 1.0;
     }
 }
 
 int main(int argc, char* argv[])
 {
-   // std::cout << "Hello World" << std::endl;
-
-    unsigned int nLoops = std::stoi(argv[1]);
     double counter = 0.0;
+   	timespec time_begin, time_end;
 
-    struct timespec debut, fin, duree;
-    clock_gettime(CLOCK_REALTIME, &debut);
-    // . . . . Section de code dont on veut mesurer la durée . . . .
-    incr(nLoops, &counter);
-    clock_gettime(CLOCK_REALTIME, &fin);
-
-    std::cout << "Counter value = " << counter << std::endl;
-
-    duree.tv_sec = fin.tv_sec - debut.tv_sec;
-    if (fin.tv_nsec < debut.tv_nsec) {
-    duree.tv_sec -= 1;
-    fin.tv_nsec += 1000000000;
+    if (argc == 1) 
+    {
+        std::cout << "Loop value requise, insert into format './execfile nLoops'" << std::endl;
+        return 1;
     }
-    duree.tv_nsec = fin.tv_nsec - debut.tv_nsec;
-    std::cout << duree.tv_sec + duree.tv_nsec/1000000000. << " seconds" << std::endl;
 
-    // volatile bool stop = false;
-    // pthread_t incrementThread;
-    // pthread_create(&incrementThread, nullptr, incrementer, (void*) &stop);
-    
-    // for (char cmd = 'r'; cmd != 's'; std::cin >> cmd)
-    // std::cout << "Type 's' to stop: " << std::flush;
-    
-    // stop = true;
-    // pthread_join(incrementThread, nullptr);
+    unsigned int nLoops = strtoul(argv[1], NULL, 10);
 
+    time_begin = timespec_now();
+    incr(nLoops, &counter);          
+    time_end = timespec_now();
+
+    std::cout << "Counter value: " << counter << std::endl;    
+    std::cout << std::fixed << std::setprecision(9) << "Time needed: " << (timespec_to_ms(time_end - time_begin)/1000) << " seconds" << std::endl; 
+    
     return 0;
-
-
 }
